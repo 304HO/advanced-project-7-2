5c4cb7f4564f0f10f448c3c91fdcf198bee08ebd
304HO <lonnie4165@gmail.com>
2022-08-30 21:53:17 +0900

    update comment, Swiper

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import Error404 from "./pages/Error404";
 import Main from "./pages/Main";
+import ReviewDetail from "./pages/ReviewDetail";
 import Test from "./pages/Test";
 
-
 export type RouteType = {
-  name: string,
-  key: string,
-  route: string,
-  component: React.ReactElement,
+  name: string;
+  key: string;
+  route: string;
+  component: React.ReactElement;
 };
 
 const routes: Array<RouteType> = [
@@ -15,19 +15,25 @@ const routes: Array<RouteType> = [
     name: "Main",
     key: "Main",
     route: "/",
-    component: <Main />,
+    component: <Main />
   },
   {
     name: "Error404",
     key: "Error404",
     route: "/404",
-    component: <Error404 />,
+    component: <Error404 />
   },
   {
     name: "Test",
     key: "Test",
     route: "/Test",
-    component: <Test />,
+    component: <Test />
+  },
+  {
+    name: "ReviewDetail",
+    key: "ReviewDetail",
+    route: "/ReviewDetail/:id",
+    component: <ReviewDetail />
   }
 ];
 
