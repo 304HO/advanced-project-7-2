c4d9faaaeca13939d38319f124bde1e618f816f8
JSC <gbs04087@gmail.com>
2022-08-30 15:56:55 +0900

    FEAT: add redux and review page

---
+++ src/components/ReviewArticle/Content.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+function Content() {
+  return (
+    <>
+      <div>asdf</div>
+    </>
+  );
+}
+
+export default Content;
